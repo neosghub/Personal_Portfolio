632c75a669e51fcb4c8845fc0b376d36e328e7d7
neosghub <165160681+neosghub@users.noreply.github.com>
2024-06-06 02:49:03 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+window.onload = function () {
+    document.querySelector("h1").style.fontSize = '20px';
+    var foo = true;
+    if(foo) {
+        var bar = foo * 2;
+        console.log(bar);
+    }
+    for(var i=0;i<10;i++){
+        console.log(i);
+    }
+    console.log(bar);
+    console.log(i); 
+    
+}
+   
